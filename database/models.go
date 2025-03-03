d43735bd8edcc899e3b6643244c1dbc6c0ec042f
Dheeraj <dyaduwanshi0@gmail.com>
2025-03-03 13:28:34 +0530

    format

--- database/models.go
+++ database/models.go
@@ -5,8 +5,8 @@ import "gorm.io/gorm"
 // Struct to store block data in db
 type Block struct {
 	gorm.Model
-	BlockNumber  int64 `json:"blockNumber"    gorm:"column:block_number"`
+	BlockNumber  int64  `json:"blockNumber"   gorm:"column:block_number"`
 	BlockHash    string `json:"blockHash"     gorm:"column:block_hash"`
-	NumberOfTxns int64	`json:"numberOfTxns"  gorm:"column:number_of_txns"`
-	Miner 		 string `json:"miner" 		  gorm:"column:miner"`
+	NumberOfTxns int64  `json:"numberOfTxns"  gorm:"column:number_of_txns"`
+	Miner        string `json:"miner" 		  gorm:"column:miner"`
 }
